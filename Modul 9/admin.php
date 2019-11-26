a9afc5ec4be69bafb6a7c6fc2d66c4d050616f7a
Fajar Nur Hidayat <48044166+L200170003@users.noreply.github.com>
2019-11-26 14:00:06 +0700

    Add files via upload

---
+++ Modul 9/admin.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <title>Selamat Datang Admin</title>
+    <style type="text/css">
+    h1 {text-shadow: 5px 5px 5px #ff0000}
+    .posisi1{position:relative ; top:15;
+    left: 25; z-index:10; color: #000000; font-family: Arial, Helvetica, sans-serif; font-size: 25pt}
+    .posisi2{position:relative; top:-22;
+    left: 28; z-index:6; color: #00ccff; font-family: Arial, Helvetica, sans-serif; font-size: 25pt}
+    .posisi3{position:relative; top:-59;
+    left: 29; z-index:6; color: #ff00ff; font-family: Arial, Helvetica, sans-serif; font-size: 25pt}
+    </style>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+    <br>
+    <center><marquee direction='down'> <h1>Selamat Datang Admin</h1> </marquee>
+    Silahkan logout dengan klik link <a href="logout2.php" class="link">Disini</a><br>
+    <img src='cat.jpg'>
+    <marquee direction='down'> <h1>Semangat Bosqiuu</h1> </marquee>
+    </center>
+    
+</body>
+</html>
---
+++ Modul 9/member.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <title>Selamat Datang Member</title>
+    <style type="text/css">
+    h1 {text-shadow: 5px 5px 5px #336b87}
+    .posisi1{position:relative ; top:15;
+    left: 25; z-index:10; color: #000000; font-family: Arial, Helvetica, sans-serif; font-size: 25pt}
+    .posisi2{position:relative; top:-22;
+    left: 28; z-index:6; color: #00ccff; font-family: Arial, Helvetica, sans-serif; font-size: 25pt}
+    .posisi3{position:relative; top:-59;
+    left: 29; z-index:6; color: #ff00ff; font-family: Arial, Helvetica, sans-serif; font-size: 25pt}
+    </style>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+    <br>
+    <center><marquee direction='up'><h1>Selamat Datang Member</h1></marquee>
+    Silahkan logout dengan klik link <a href="logout2.php" class="link">Disini</a><br>
+    <img src='cat2.jpg'>
+    </center>
+</body>
+</html>
